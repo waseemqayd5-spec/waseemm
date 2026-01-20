2929a3ffe631dfc6866838ccfaf484472740cffd
waseemqayd5-spec <waseemqayd5@gmail.com>
2026-01-20 15:44:00 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -962,4 +962,6 @@ if __name__ == '__main__':
         os.makedirs('data')
         print("✅ تم إنشاء مجلد data")
 
-    app.run(debug=True,port = 5000)
+app.run(host="0.0.0.0", port=10000)
+    
+    
